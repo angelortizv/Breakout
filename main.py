e8ab3bb0e495b355850a87f0e1dab6f24694f333
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-07-11 00:20:07 -0600

    [INIT] Setup Project

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Breakout:
+    def __init__(self):
+        pygame.init()
+
+
+    def runBreakout(self):
+        print("Breakout")
+
+
+if __name__ == '__main__':
+    game = Breakout()
+    game.runBreakout()
